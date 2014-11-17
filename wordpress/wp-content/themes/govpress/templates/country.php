9e5bbe9cffae105dc238fafc90bac3601f7b0abb
jeromejesse <jerome.lionel.jesse@gmail.com>
2014-11-17 15:40:45 +0100

    retrieve entry

--- wordpress/wp-content/themes/govpress/templates/country.php
+++ wordpress/wp-content/themes/govpress/templates/country.php
@@ -166,22 +166,7 @@ $key_country = get_post_meta( get_the_ID(), 'country', true );
             <li>Individual items included in an (original or unoriginal) database may be independently protected. In this case, the Public Domain Helper Tool should also be applied to each of these individually.</li>
         </ul>
 		</div><!-- #content -->
-        <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-            <header class="entry-header">
-                <h1 class="entry-title"><?php the_title(); ?></h1>
-            </header><!-- .entry-header -->
-
-            <div class="entry-content">
-                <?php echo get_the_content(); ?>
-                <?php
-                    wp_link_pages( array(
-                        'before' => '<div class="page-links">' . __( 'Pages:', 'govpress' ),
-                        'after'  => '</div>',
-                    ) );
-                ?>
-            </div><!-- .entry-content -->
-            <?php edit_post_link( __( 'Edit', 'govpress' ), '<footer class="entry-meta"><span class="edit-link">', '</span></footer>' ); ?>
-        </article><!-- #post-## -->
+       
 	</div><!-- #primary -->
 
 <?php
