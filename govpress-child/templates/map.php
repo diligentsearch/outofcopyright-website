7348184cf525befa02d0106ab5bad087990eed26
mzeinstra <mzeinstra@MacBook-Air-Maarten-8.local>
2014-11-20 15:19:06 +0100

    Added map.php template with /lib/ requirements
    Added a map template for the child theme that uses d3.js maps for
    another research project to be presented on OutOfCopyright.eu

---
+++ govpress-child/templates/map.php
@@ -0,0 +1,79 @@
+<?php
+/**
+ * Template Name: Map
+ *
+ * @package WordPress
+ * @subpackage govpress
+ * @since govpress 1.0
+ */
+get_header(); 
+
+$key_country = get_post_meta( get_the_ID(), 'country', true );
+$location = get_stylesheet_directory_uri();
+$key_map = get_post_meta( get_the_ID(), 'map', true );
+                
+?>
+
+	<div id="primary" class="content-area">
+		<div id="content" class="site-content" role="main">
+        <h1 style="font-size: 25px;"><?php echo the_title(); ?></h1>
+		
+		<!-- Map -->
+		<div class="col-width" style="margin-bottom:1em;">
+			<div id="map" style="background-color: #FFFFFF; width: 100%; height: 500px; border: 1px solid #898989; margin: 0 0 1em 0;"></div>
+			<div id="map-tooltip" style="height:auto"></div>
+      	</div>	
+		<link rel="stylesheet" href="<?php echo $location?>/lib/css/simple-map-d3.css">
+		<script type="text/javascript" src="<?php echo $location?>/lib/js/jquery/jquery.min.js"></script>
+		<script type="text/javascript" src="<?php echo $location?>/lib/js/d3/d3.min.js"></script>
+		<script type="text/javascript" src="<?php echo $location?>/lib/js/topojson/topojson.js"></script>
+		<script type="text/javascript" src="<?php echo $location?>/lib/js/simple-map-d3/simple-map-d3.js"></script>
+
+		<script type="text/javascript">
+		  (function($) {
+			  $(document).ready(function() {
+
+				  var europePopMap = SimpleMapD3({
+					  container: '#map',
+					  datasource: '<?php echo $location?>/lib/data/europe.json',
+					  colorSet: ['rgb(255,0,0)', 'rgb(0,255,0)', 'rgb(255,128,0)'],
+					  colorProperty: 'scale',
+					  colorScale: 'ordinal',
+					  colorOn: true,
+					  legendOn: false,
+					  colorProperty: 'scale',
+					  colorReverse: true,
+					  projection: 'conicConformal',
+					  stylesBackground: 'background-color: #FFF;',
+					  canvasDragOn: true,
+
+					  tooltipContent: function(d) {
+						  var p = d.properties;
+						  document.getElementById("map-tooltip").innerHTML = '<h5>' + p.country + '</h5>' + "<p>" + p.result + "</p>" + "<p>" + p.value + "</p>";
+
+
+					  }
+				  });
+
+
+			  });
+		  })(jQuery);
+		</script>
+		<!-- END Map -->
+       <div class="col-width clear"> 
+        <?php
+        if (have_posts()) :
+           while (have_posts()) :
+              the_post();
+                 the_content();
+           endwhile;
+        endif;
+        ?>
+    </div> <!-- colwidth -->
+		</div><!-- #content -->
+       
+	</div><!-- #primary -->
+
+<?php
+
+get_footer();
