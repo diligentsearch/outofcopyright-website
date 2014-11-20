a5b71b153735f1a22e670cad7994b9c6bd8df90e
jeromejesse <jerome.lionel.jesse@gmail.com>
2014-11-20 17:44:23 +0100

    Langues test

--- govpress-child/functions.php
+++ govpress-child/functions.php
@@ -10,11 +10,4 @@ function add_login_logout_link($items, $args) {
     return $items;
 }
 
-add_filter( 'wp_nav_menu_items', 'qtrans_generateLanguageSelectCode_items', 10, 2);
-function qtrans_generateLanguageSelectCode_items($items, $args) {
-	$homelink = '<li>' . qtrans_generateLanguageSelectCode('dropdown') . '</li>';
-	$test = qtrans_generateLanguageSelectCode('dropdown');
-	$items .= $homelink;
-	echo 'test111 '.$test;
-	return $items;
-}
+qtrans_generateLanguageSelectCode('dropdown');
