7e51b3d1ec8f2395dd7a8bed8feb158ab8e878f4
jeromejesse <jerome.lionel.jesse@gmail.com>
2014-11-20 16:18:54 +0100

    fixx second forms

--- govpress-child/templates/country-step-by-step.php
+++ govpress-child/templates/country-step-by-step.php
@@ -16,121 +16,149 @@ $key_country = get_post_meta( get_the_ID(), 'country', true );
 		<div id="content" class="site-content" role="main">
         <script>
             var inputs;
-        function submit(){
+            var actualInput;
+            function submit(){
 
-            $(".alert").hide();
+                $(".alert").hide();
+                if($("#"+actualInput).val() != ""){
+                    var responses = new Object();
+                    var errors = [];
 
-            var responses = new Object();
-            var errors = [];
-            $(".questionPart").hide();
-            for(var i = 0; i < inputs.length; i++){
-                $("#"+inputs[i]).hide
-                if($("#"+inputs[i]).val() != ""){
-                    var response = $("#"+inputs[i]).val();
-                    eval("responses."+inputs[i]+" = response;");
-                }
-                var trad = getTraduction(file.default_language, inputs[i]);
-                errors[inputs[i]] = trad+' missing';
-            }
-
-            responses = JSON.stringify(responses);
-            var result = walk($( "#typeOfWork" ).val(), responses);
-
-            if(result.error == 1){
-                console.log(result);
-                newQuestion(result.waiting_response);
-            }else{
-                $("#next").hide();
-                $("#result").html(result);
-                $(".alert-success").show();
-            }
-        }
-
-        function reset(){
-            $("#next").hide();
-            $("#reset").hide();
-            $(".alert-success").hide();
-            $("#forms").html("");
-            $("#typeOfWork").val("");
-        }
-        
-        $(document).on('change','#typeOfWork',function(){
-            $("#forms").html("");
-            if($( "#typeOfWork" ).val() != ""){
-                $.post( "/node", { country: 'Belgium', name: file.default_language+'.json', action: 'read' } )
-                .done(function( dataTrad ) {
-                    traductionData = dataTrad;
-                    inputs = getInputs($( "#typeOfWork" ).val());
-                    if(inputs.length > 0){
-                        newQuestion(inputs[0]);
+                    
+                    for(var i = 0; i < inputs.length; i++){
+                        $("#"+inputs[i]).hide;
+                        if($("#"+inputs[i]).val() != ""){
+                            var response = $("#"+inputs[i]).val();
+                            eval("responses."+inputs[i]+" = response;");
+                        }
+                        var trad = getTraduction(file.default_language, inputs[i]);
+                        errors[inputs[i]] = trad+' missing';
                     }
+
+                    responses = JSON.stringify(responses);
+                    var result = walk($( "#typeOfWork" ).val(), responses);
+
+                    if(result.error == 3){
+                        $("#error").html("Response is not a number.");
+                        $(".alert-warning").show();
+                    }
+                    else{
+                        if(result.error == 1){
+                            console.log(result);
+                            $(".questionPart").hide();
+                            newQuestion(result.waiting_response);
+                            
+                        }else{
+                            $("#result").html(result);
+                            $(".alert-success").show();
+                            $(".questionPart").hide();
+                        }
+                    }
+                    
+                    $("#next").hide();
+                }
+                
+            }
+
+            function reset(){
+                $("#next").hide();
+                $("#reset").hide();
+                $(".alert-success").hide();
+                $("#forms").html("");
+                $("#typeOfWork").val("");
+            }
+
+            $(document).on('change','.questionInput',function(){
+                changeInput();
+            });
+
+            
+
+            function changeInput(){
+                if($("#"+actualInput).val() != ""){
                     $("#next").show();
-                    $("#reset").show();
-                });
-            }else{
-                reset();
+                }else{
+                    $("#next").hide();
+                }
             }
             
-          });   
-
-        $(function() {
-
-            $("#secondary").hide();
-
-            $.post( "/node", { country: 'Belgium', name: 'Belgium.json', action: 'read' } )
-            .done(function( data ) {
-                file = data;
-
-
-
-                //getInputs(id_subgraph);
-
-                var listSubgraph = getListSubgraph();
-
-                for(var i = 0; i < listSubgraph.length; i++){
-                    $('#typeOfWork')
-                         .append($("<option></option>")
-                         .attr("value",i)
-                         .text(listSubgraph[i])); 
+            $(document).on('change','#typeOfWork',function(){
+                $("#forms").html("");
+                if($( "#typeOfWork" ).val() != ""){
+                    $.post( "/node", { country: 'Belgium', name: file.default_language+'.json', action: 'read' } )
+                    .done(function( dataTrad ) {
+                        traductionData = dataTrad;
+                        inputs = getInputs($( "#typeOfWork" ).val());
+                        if(inputs.length > 0){
+                            newQuestion(inputs[0]);
+                        }
+                        $("#reset").show();
+                    });
+                }else{
+                    reset();
                 }
-            });
+                
+              });   
 
-            $('.close').click(function(){
-                $(".alert").hide();
-            });
-        });
+            $(function() {
 
-        function newQuestion(input){
-            var trad = getTraduction(file.default_language, 'question_'+input);
-            var datapoint = getResponseById(input);
-            var inputHTML= "";
-            switch(datapoint.type) {
-                case 'numeric':
-                    inputHTML = '<input type="text" class="col-sm-12" name="'+input+'" id="'+input+'" placeholder="'+trad+'"/>';
-                    break;
-                case 'list':
-                    inputHTML = '<select name="'+input+'" id="'+input+'" class="form-control" style=" margin-bottom: 15px; ">'
-                    inputHTML += '<option value="">Select a response</option>';
-                    for(var j = 0; j < datapoint.set.length; j++){
-                        inputHTML += '<option value="'+datapoint.set[j]+'">'+datapoint.set[j]+'</option>';
+                $.post( "/node", { country: 'Belgium', name: 'Belgium.json', action: 'read' } )
+                .done(function( data ) {
+                    file = data;
+
+
+
+                    //getInputs(id_subgraph);
+
+                    var listSubgraph = getListSubgraph();
+
+                    for(var i = 0; i < listSubgraph.length; i++){
+                        $('#typeOfWork')
+                             .append($("<option></option>")
+                             .attr("value",i)
+                             .text(listSubgraph[i])); 
                     }
-                    inputHTML += '</select>';
-                    break;
-                default:
-                    inputHTML = '';
-            } 
+                });
 
-            $("#forms").append('\
-                <div class="row margin-top questionPart">\
-                    <div class="col-sm-4">\
-                        <label for="'+input+'" class="control-label">'+trad+'</label>\
+                $('.close').click(function(){
+                    $(".alert").hide();
+                });
+            });
+
+            function newQuestion(input){
+                var trad = getTraduction(file.default_language, 'question_'+input);
+                var datapoint = getResponseById(input);
+                var inputHTML= "";
+                actualInput = input;
+                switch(datapoint.type) {
+                    case 'numeric':
+                        inputHTML = '<input type="text" class="col-sm-12 questionInput" name="'+input+'" id="'+input+'" placeholder="'+trad+'" onkeypress="changeInput()"/>';
+                        break;
+                    case 'list':
+                        inputHTML = '<select name="'+input+'" id="'+input+'" class="form-control questionInput" style=" margin-bottom: 15px; ">'
+                        inputHTML += '<option value="">Select a response</option>';
+                        for(var j = 0; j < datapoint.set.length; j++){
+                            inputHTML += '<option value="'+datapoint.set[j]+'">'+datapoint.set[j]+'</option>';
+                        }
+                        inputHTML += '</select>';
+                        break;
+                    default:
+                        inputHTML = '';
+                } 
+
+                $("#forms").append('\
+                    <div class="row margin-top questionPart">\
+                        <div class="col-sm-4">\
+                            <label for="'+input+'" class="control-label">'+trad+'</label>\
+                        </div>\
+                        <div class="col-sm-8">\
+                            '+inputHTML+'\
+                        </div>\
                     </div>\
-                    <div class="col-sm-8">\
-                        '+inputHTML+'\
-                    </div>\
-                </div>\
-                ');
-        }
+                    ');
+                $("#next").hide();
+            }
+
         </script>
         <h1 style="font-size: 25px;"><?php echo $key_country; ?></h1>
         <br/>
@@ -164,6 +192,11 @@ $key_country = get_post_meta( get_the_ID(), 'country', true );
             <div class="alert alert-dismissable alert-danger margin-top" style="display:none;">
                 <button type="button" class="close" data-dismiss="alert">×</button>
                 <p id="resultdanger"></p>
+            </div>
+            <div class="alert alert-dismissable alert-warning margin-top" style="display:none;">
+                <button type="button" class="close" data-dismiss="alert">×</button>
+                <h4>Warning!</h4>
+                <p id="error"></p>
             </div>
         </div>
         <?php
