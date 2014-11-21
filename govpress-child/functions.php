124b7cbda994291b690c6ef6cf2ba77146193420
jeromejesse <jerome.lionel.jesse@gmail.com>
2014-11-21 09:26:25 +0100

    Langues test

--- govpress-child/functions.php
+++ govpress-child/functions.php
@@ -16,7 +16,10 @@ function qtrans_generateLanguageSelectCode_items($items, $args) {
 	if($_SESSION['countMenu'] == 0){
 		$items .= qtrans_generateLanguageSelectCode('image');
 		$_SESSION['countMenu']++;
+		echo "test";
+		print_r(qtrans_generateLanguageSelectCode('image'));
+		echo "test";
 	}
-	print_r($items);
+
 	return $items;
 }
