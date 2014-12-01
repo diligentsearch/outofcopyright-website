43a61c345a88b168099171957c6acd6fc8860afb
jeromejesse <jerome.lionel.jesse@gmail.com>
2014-12-01 11:13:18 +0100

    no-cache

--- govpress-child/header.php
+++ govpress-child/header.php
@@ -17,6 +17,11 @@
 <html <?php language_attributes(); ?>>
 <!--<![endif]-->
 <head>
+<meta http-equiv="cache-control" content="max-age=0" />
+<meta http-equiv="cache-control" content="no-cache" />
+<meta http-equiv="expires" content="0" />
+<meta http-equiv="expires" content="Tue, 01 Jan 1980 1:00:00 GMT" />
+<meta http-equiv="pragma" content="no-cache" />
 <meta charset="<?php bloginfo( 'charset' ); ?>">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <title><?php wp_title( '|', true, 'right' ); ?></title>
