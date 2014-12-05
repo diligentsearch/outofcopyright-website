bdfbb10831c64da5453171a4983dc0c6005fcd8c
jeromejesse <jerome.lionel.jesse@gmail.com>
2014-12-05 18:01:13 +0100

    change style

--- govpress-child/templates/countries-list.php
+++ govpress-child/templates/countries-list.php
@@ -26,7 +26,7 @@ get_header();
 	<div id="primary" class="content-area">
 		<div id="content" class="site-content" role="main">
 			<div class="col-sm-2" style="height: 100%;">
-        <div class="row" style="border-bottom: solid 1px #CFCFCF;border-right: solid 1px #000000;background-image: linear-gradient(to bottom, #ffffff, #f2f2f2);">
+        <div class="row" style="border-bottom: solid 1px #CFCFCF;border-right: solid 3px #CFCFCF;background-image: linear-gradient(to bottom, #ffffff, #f2f2f2);">
             <div class="col-sm-12 text-center margin-bottom height-40 padding-top-15 grey-color" style="font-weight: bold;" >
                 COUNTRIES
             </div>
@@ -39,7 +39,7 @@ get_header();
         </div>
     </div>
     <div class="col-sm-7">
-        <div class="row" style="border-bottom: solid 1px #CFCFCF;border-right: solid 1px #000000;background-image: linear-gradient(to bottom, #ffffff, #f2f2f2);">
+        <div class="row" style="border-bottom: solid 1px #CFCFCF;border-right: solid 3px #CFCFCF;background-image: linear-gradient(to bottom, #ffffff, #f2f2f2);">
             <div class="col-sm-12 margin-bottom height-40 padding-top-15 grey-color" id="flowchart-title" style="font-weight: bold;">
                 FLOWCHART
             </div>
