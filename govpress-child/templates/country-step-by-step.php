7791e669dde0ca9c6c9a528671ed0ecfa41a87fb
jeromejesse <jerome.lionel.jesse@gmail.com>
2014-12-15 10:56:54 +0100

    hide popover

--- govpress-child/templates/country-step-by-step.php
+++ govpress-child/templates/country-step-by-step.php
@@ -174,6 +174,15 @@ if(isset($_GET['country'])){
 
             });
 
+            $('body').on('click', function (e) {
+                //did not click a popover toggle, or icon in popover toggle, or popover
+                if ($(e.target).data('toggle') !== 'popover'
+                    && $(e.target).parents('[data-toggle="popover"]').length === 0
+                    && $(e.target).parents('.popover.in').length === 0) { 
+                    $('[data-toggle="popover"]').popover('hide');
+                }
+            });
+
             function changeLangue(){
                 getData("<?php echo $key_country; ?>", $("#langues").val(), function( dataTrad ) {
                     traductionData = dataTrad;
