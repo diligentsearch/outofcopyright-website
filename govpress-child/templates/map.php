62027d7c4977aa461a2f09b38afbcdacd32908d4
mzeinstra <mzeinstra@MacBook-Air-Maarten-8.local>
2014-12-16 13:58:55 +0100

    Updated map.php
    Updated layout to conform to the requirements of of the project.

--- govpress-child/templates/map.php
+++ govpress-child/templates/map.php
@@ -8,70 +8,63 @@
  */
 get_header(); 
 
-$key_country = get_post_meta( get_the_ID(), 'country', true );
 $location = get_stylesheet_directory_uri();
 $key_map = get_post_meta( get_the_ID(), 'map', true );
                 
 ?>
 
-	<div id="primary" class="content-area">
-		<div id="content" class="site-content" role="main">
-        <h1 style="font-size: 25px;"><?php echo the_title(); ?></h1>
+<div id="primary" class="content-area">
+	<div id="content" class="site-content" role="main">
+		<h1 style="font-size: 25px;"><?php echo the_title(); ?></h1>
+
+		<div class="entry-content">
+			<?php if ( have_posts() ) : while ( have_posts() ) : the_post();
+				the_content();
+				endwhile;
+			endif; ?>
 		
-		<!-- Map -->
-		<div class="col-width" style="margin-bottom:1em;">
-			<div id="map" style="background-color: #FFFFFF; width: 100%; height: 500px; border: 1px solid #898989; margin: 0 0 1em 0;"></div>
-			<div id="map-tooltip" style="height:auto"></div>
-      	</div>	
-		<link rel="stylesheet" href="<?php echo $location?>/lib/css/simple-map-d3.css">
-		<script type="text/javascript" src="<?php echo $location?>/lib/js/jquery/jquery.min.js"></script>
-		<script type="text/javascript" src="<?php echo $location?>/lib/js/d3/d3.min.js"></script>
-		<script type="text/javascript" src="<?php echo $location?>/lib/js/topojson/topojson.js"></script>
-		<script type="text/javascript" src="<?php echo $location?>/lib/js/simple-map-d3/simple-map-d3.js"></script>
-
-		<script type="text/javascript">
-		  (function($) {
-			  $(document).ready(function() {
-
-				  var europePopMap = SimpleMapD3({
-					  container: '#map',
-					  datasource: 'https://rawgit.com/outofcopyright/outofcopyright-maps/master/<?php echo $key_map; ?>.json',
-					  colorSet:  ['rgb(255,255,255)','rgb(250,170,0)','rgb(0,109,145)'],
-					  colorProperty: 'scale',
-					  colorScale: 'ordinal',
-					  colorOn: true,
-					  colorReverse: false,
-					  legendOn: false,
-					  projection: 'conicConformal',
-					  stylesBackground: 'background-color: #FFF;',
-					  canvasDragOn: true,
-
-					  tooltipContent: function(d) {
-						  var p = d.properties;
-						  document.getElementById("map-tooltip").innerHTML = '<h5>' + p.country + '</h5>' + "<p>" + p.result + "</p>" + "<p>" + p.value + "</p>";
-
-
-					  }
-				  });
-
-
-			  });
-		  })(jQuery);
-		</script>
-		<!-- END Map -->
-       <div class="col-width clear"> 
-        <?php
-        if (have_posts()) :
-           while (have_posts()) :
-              the_post();
-                 the_content();
-           endwhile;
-        endif;
-        ?>
-    </div> <!-- colwidth -->
-		</div><!-- #content -->
-       
-	</div><!-- #primary -->
+			<!-- Map -->
+			<div class="col-width" style="margin-bottom:1em;">
+				<div id="map" style="background-color: #FFFFFF; width: 60%; height: 500px; border: 1px solid #898989; margin: 0 0 1em 0; float:left;"></div>
+					<div id="map-tooltip" style="height:auto;float: right;width: 40%;padding:1em;"></div>
+			</div>	
+			<link rel="stylesheet" href="<?php echo $location?>/lib/css/simple-map-d3.css">
+			<script type="text/javascript" src="<?php echo $location?>/lib/js/jquery/jquery.min.js"></script>
+			<script type="text/javascript" src="<?php echo $location?>/lib/js/d3/d3.min.js"></script>
+			<script type="text/javascript" src="<?php echo $location?>/lib/js/topojson/topojson.js"></script>
+			<script type="text/javascript" src="<?php echo $location?>/lib/js/simple-map-d3/simple-map-d3.js"></script>
+			<script type="text/javascript">
+				(function($) {
+					$(document).ready(function() {
+						var europePopMap = SimpleMapD3({
+							container: '#map',
+							datasource: 'https://rawgit.com/outofcopyright/outofcopyright-maps/master/<?php echo $key_map; ?>.json',
+							colorSet:  ['rgb(255,255,255)','rgb(250,170,0)','rgb(0,109,145)'],
+							colorProperty: 'scale',
+							colorScale: 'ordinal',
+							colorOn: true,
+							colorReverse: false,
+							legendOn: false,
+							projection: 'conicConformal',
+							stylesBackground: 'background-color: #FFF;',
+							canvasDragOn: true,
+						
+							tooltipContent: function(d) {
+								var p = d.properties;
+								if (p.result != "") {
+									document.getElementById("map-tooltip").innerHTML = '<h2>' + p.country + '</h2>' + "<p>" + p.result + "</p>" + "<p>" + p.value + "</p>";
+								} else {
+									document.getElementById("map-tooltip").innerHTML = '<h2>' + p.country + '</h2>' + "<p>" + p.value + "</p>";
+								}
+							}
+						});
+					});
+				})(jQuery);
+			</script>
+			<!-- END Map -->
+		</div><!-- .entry-content -->	
+	</div><!-- #content -->
+</div><!-- #primary -->
 
 <?php
 
