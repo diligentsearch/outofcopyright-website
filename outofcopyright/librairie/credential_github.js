c40f28725b1caa8d1a7dd4c462dfb286c8014b69
Damien <nfsdamien@NFSs-MBP.fritz.box>
2015-03-03 11:35:54 +0100

    OFC - change copyright

--- outofcopyright/librairie/credential_github.js
+++ outofcopyright/librairie/credential_github.js
@@ -1,5 +1,5 @@
 //Donnée de connexion au compte github basique
 var USERNAME = "jeromejesse"
   , PASSWORD = "finalfantasy10"
-  , REPONAME = 'outofcopyright-test'
+  , REPONAME = 'outofcopyright-files'
   , BRANCH   = 'master';
